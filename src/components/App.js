8650d0e09d205a92f7ccf7130d65274eefb80729
Prashant Kumar <prashant.aman9@gmail.com>
2024-08-04 12:42:09 +0530

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './../styles/App.css'
+const fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig']
 
 const App = () => {
-	return <div>{/* Do not remove the main div */}</div>
+  const [filteredFruits, setFilteredFruits] = useState(fruits)
+  const [searchText, setSearchText] = useState('')
+  
+  useEffect(() => {
+    setFilteredFruits(fruits.filter((fruit)=>fruit.startsWith(searchText)))
+  }, [searchText])
+  
+  const handleChange = (e) => {
+    setSearchText(e.target.value)
+  }
+
+	return (
+		<div>
+			{/* Do not remove the main div */}
+			<h1>Search item</h1>
+			<input type='search' value={searchText} onChange={handleChange}/>
+			<ul>
+				{filteredFruits.map((fruit, index) => (
+					<li key={index}>{fruit}</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default App
